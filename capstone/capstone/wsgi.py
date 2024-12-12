77a12376a1b7ae09e2296bfe1cba3e71181a1f6e
Sundess <sundess17@gmail.com>
2024-12-12 12:28:53 +0545

    fix: changes to wgsi file

--- capstone/capstone/wsgi.py
+++ capstone/capstone/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.capstone.settings')
 
 application = get_wsgi_application()
 
